e95cfea99a833326d0ee9a9deb8dc4afd466821e
asprtam <44498326+asprtam@users.noreply.github.com>
2019-11-25 11:51:44 +0100

    Add files via upload

---
+++ precache-manifest.8ace02dd900d7aa9cc21af4f9af207ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89eb70b7e64e37280f1c804c4e90904",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e11fd2a03350b5d1e2e9",
+    "url": "/static/css/main.cbf24c57.chunk.css"
+  },
+  {
+    "revision": "b1e0d94db35f88503d50",
+    "url": "/static/js/2.fc74f443.chunk.js"
+  },
+  {
+    "revision": "e11fd2a03350b5d1e2e9",
+    "url": "/static/js/main.8f9b1007.chunk.js"
+  },
+  {
+    "revision": "7b7a7c17413b22df186a",
+    "url": "/static/js/runtime-main.398a2ef4.js"
+  },
+  {
+    "revision": "0cec2c4e9e8954c46d6643d9ff5c058f",
+    "url": "/static/media/bg.0cec2c4e.jpg"
+  },
+  {
+    "revision": "b87dea10dca538ce1f8ebf51b17a5e31",
+    "url": "/static/media/kawiarnia.b87dea10.png"
+  },
+  {
+    "revision": "4ed774b6b57f8f048abe62247aee3a19",
+    "url": "/static/media/main-img2.4ed774b6.png"
+  },
+  {
+    "revision": "96a74e301f95deea5866e636d3406f4d",
+    "url": "/static/media/sunGradient.96a74e30.png"
+  }
+]);
